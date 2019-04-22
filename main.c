49c4273eef843a091256ac6ce0be473c4a057061
Muhammed Eldeeb <meldeeb1998@gmail.com>
2019-04-22 20:49:25 +0200

    write on a file

--- main.c
+++ main.c
@@ -3,9 +3,31 @@
 #include <string.h>
 #include <mpich/mpi.h>
 
+int candidats = 3;
+int voters = 3;
+
+
+void writeRandomVotes(int* vote){
+    FILE* myfile;
+    myfile = fopen("myfile.txt","a");
+    int c = 0;
+    for (; c < candidats ; c++)
+    {
+        fprintf (myfile, "%d ",vote[c]);
+    }
+    fprintf(myfile , "\n");
+
+    fclose(myfile);
+}
+
+
+
 int main(int argc , char* argv[]){
 
-printf("hello world!");
+   int vote[] = {1 , 2 , 3};
+   writeRandomVotes(vote);
+   writeRandomVotes(vote);
+   writeRandomVotes(vote);
 
     return 0;
 }
