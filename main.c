2c51ebf81931dc7e836d86b0d620a22a5f081a63
Mohamed-Kembeshawey <35874384+Mohamed-Kembeshawey@users.noreply.github.com>
2019-04-22 23:10:25 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,9 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+#include <mpich/mpi.h>
 
-int candidates=45;
+int candidats = 0;
+int voters = 0;
 
+int* myrandom(int num)
+{
+    int i,j;
+    int *arr= malloc(sizeof(int)*num);
+    for (i = 0; i < num; i++)
+    {
+        arr[i] = rand() % num + 1;
+        for(j=0;j<i;j++)
+        {
+            if(arr[j]==arr[i])
+                --i;
+        }
+    }
+    return arr;
+}
+
+
+void writeRandomVotes(int* vote){
+    FILE* myfile;
+    myfile = fopen("myfile.txt","a");
+    int c = 0;
+    for (; c < candidats ; c++)
+    {
+        fprintf (myfile, "%d ",vote[c]);
+    }
+    fprintf(myfile , "\n");
+
+    fclose(myfile);
+}
+
+void inializeTheVotes(){
+    printf("enter number of candidats : ");
+    scanf("%d" , &candidats);
+    printf("Enter the number of voters : ");
+    scanf("%d" , &voters);
+
+    int num = candidats , v;
+
+    int *arr= malloc(sizeof(int)*num);
+    for(v = 1; v <= voters ;v++)
+    {
+        arr=myrandom(num);
+        writeRandomVotes(arr);
+    }
+}
 int * random(int num)
 {
     int i,j;
@@ -33,7 +81,7 @@ int getDigits(int num)
 void convert(int num)
 {
     int size,copy,i,rem;
-    size=getDigits(candidates);
+    size=getDigits(candidats);
     char* arr=malloc(sizeof(char)*size);
     copy=getDigits(num);
     printf("%d\n",size);
@@ -54,16 +102,9 @@ void convert(int num)
     }
     /*OR you can print the whole array here */
 }
-int main()
-{
-    /*int i,j,num =5;
-    int *arr= malloc(sizeof(int)*num);
-    for(i=0;i<10;i++)
-    {
-        arr=random(num);
-        for(int j=0;j<num;j++)
-            printf("%d ",arr[j]);
-        printf("\n");
-    }*/
+int main(int argc , char* argv[]){
+
+    inializeTheVotes();
+
     return 0;
 }
