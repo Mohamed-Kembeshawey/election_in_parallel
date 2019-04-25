c709e0f750a47e008573f3f3a7af81e7e5345ccc
Mohamed-Kembeshawey <mohamed.mk586@gmail.com>
2019-04-25 15:22:15 +0200

    test

--- main.c
+++ main.c
@@ -69,24 +69,21 @@ void convert(int num)
     size=getDigits(candidats);
     char* arr=malloc(sizeof(char)*size);
     copy=getDigits(num);
-    printf("%d\n",size);
-    printf("%d\n",copy);
     for(i=0;i<size-copy;i++)
-        arr[i]='0'; //you can print 0 in file and do not use array
+        arr[i]='0'; 
     for(i=size-1;i>=size-copy;i--)
     {
         rem=num/10;
         rem=num-(rem*10);
         if(rem==0)
-            arr[i]=(char)(num/10)+48;//you can print in file here and did not use array
+            arr[i]='0';
         else
         {
-            arr[i]=(char)rem+48;//and here
+            arr[i]=(char)rem+48;
             num-=rem;
         }
+         num/=10;
     }
-    /*OR you can print the whole array here */
-}
 int main(int argc , char* argv[]){
 
 inializeTheVotes();
